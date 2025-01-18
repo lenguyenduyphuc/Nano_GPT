de1eadf0ac23c54f09f0fcb8998c065f3581f7af
NguyenDuyPhucLe <duyphuclenguyen@gmail.com>
2025-01-17 19:05:25 -0500

    Update

--- train_gpt2.py
+++ train_gpt2.py
@@ -80,11 +80,11 @@ class GPT(nn.Module):
     super().__init__()
     self.config = config
 
-    self.transformer = nn.Module(dict(
-      wte = nn.Embedding(config.vocab_size, config.n_embd),
+    self.transformer = nn.ModuleDict(dict(
+      wte = nn.Embedding(config.vacab_size, config.n_embd),
       wpe = nn.Embedding(config.block_size, config.n_embd),
-      h = nn.ModuleList(Block(config) for _ in range(config.n_layer)),
-      lm_f = nn.LayerNorm(config.n_embd),
+      h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+      ln_f = nn.LayerNorm(config.n_embd),
     ))
     self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
   
