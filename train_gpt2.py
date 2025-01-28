2117d7bcd2e40c6c5a9e14f954960a22af3d36f7
Phuc <duyphuclenguyen@gmail.com>
2025-01-27 22:51:30 -0500

    Update loss

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import math
 import time
 from transformers import GPT2LMHeadModel
@@ -96,7 +97,7 @@ class GPT(nn.Module):
     # weight sharing scheme
     self.transformer.wte.weight = self.lm_head.weight
   
-  def forward(self, idx):
+  def forward(self, idx, targets = None):
     # idx is of shape (B, T)
     B, T = idx.size()
     assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
@@ -111,7 +112,10 @@ class GPT(nn.Module):
     # forward the final layernorm and the classifier
     x = self.transformer.ln_f(x)
     logits = self.lm_head(x) # (B, T, vocab_size)
-    return logits
+    loss = None
+    if targets is not None:
+      loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+    return logits, loss
     
 
   #loading the weight
@@ -164,17 +168,45 @@ class GPT(nn.Module):
     return model
 
 #------------------
+# detect device
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = "mps"
+print(f"Using device: {device}")
+
+enc = tiktoken.get_encoding('gpt2')
+with open('input.txt', 'r') as f:
+  text = f.read()
+text = text[:1000]
+tokens = enc.encode(text)
+B,T = 4, 32
+buf = torch.tensor(tokens[:(B*T) + 1])
+buf = buf.to(device)
+x = buf[:-1].view(B,T)
+y = buf[1:].view(B,T)
+
+# model = GPT.from_pretrained('gpt2')
+model = GPT(GPTConfig())
+model.to(device)
+# logits, loss = model(x,y)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+  optimizer.zero_grad()
+  logits, loss = model(x, y)
+  loss.backward()
+  optimizer.step()
+  print(f"step {i}, loss: {loss.item()}")
+
+sys.exit(0)
+
 num_return_sequences = 5
 max_length = 30
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = GPT.from_pretrained('gpt2')
-model.eval()
-model.to(device)
-
 #prefix token
-enc = tiktoken.get_encoding('gpt2')
-tokens = enc.encode("Hello, I am a LLM")
+tokens = enc.encode("Hello, I am a doctorate student at Harvard")
 tokens = torch.tensor(tokens, dtype = torch.long) # (8, )
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
 x = tokens.to(device) 
