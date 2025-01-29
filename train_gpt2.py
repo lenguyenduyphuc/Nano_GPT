689ceca050084e61e43e2d8f0a78d6dc6ec6c438
Phuc <duyphuclenguyen@gmail.com>
2025-01-29 12:04:54 -0500

    TF16 utilize

--- train_gpt2.py
+++ train_gpt2.py
@@ -226,38 +226,48 @@ torch.manual_seed(1337)
 if torch.cuda.is_available():
     torch.cuda.manual_seed(1337)
 
-train_loader = DataLoaderLite(B=4, T=32)
+train_loader = DataLoaderLite(B=8, T=1024)
+
+torch.set_bfloat16_matmul_precision('high')
+
+# get logits
 model = GPT(GPTConfig())
 model.to(device)
 
+# optimize!
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
+    t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
-    print(f"step {i}, loss: {loss.item()}")
+    torch.cuda.synchronize()
+    t1 = time.time()
+    dt = (t1 - t0) * 1000 # time differenes in ms
+    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 -t0)
+    print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f} tok")
 
 sys.exit(0)
 
 num_return_sequences = 5
 max_length = 30
 
-#prefix token
+# prefix token
 tokens = enc.encode("Hello, I am a doctorate student at Harvard")
 tokens = torch.tensor(tokens, dtype = torch.long) # (8, )
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
 x = tokens.to(device) 
 
-#generates! x is (B, T) = (5,8)
+# generates! x is (B, T) = (5,8)
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
 while (x.size(1) < max_length):
     # forward the model to get the grad:
     with torch.no_grad():
-        logits = model(x) #(B, T, vocab_size)
+        logits = model(x) # (B, T, vocab_size)
         # take the logits at the last pos
         logits = logits[:, -1, :]
         # get the probs
